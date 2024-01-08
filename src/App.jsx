c09d8e5136cca74094b736fe7fa61a601e00e665
mohammad erfany <98615272+merfdev@users.noreply.github.com>
2024-01-08 19:55:02 +0330

    create component page and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import ProductPage from "./pages/ProductPage";
+import NotFoundPage from "./pages/404";
+import DetailsPage from "./pages/DetailsPage";
+import CheckoutPage from "./pages/CheckoutPage";
 function App() {
   return (
-    <>
-      <h1>hey</h1>
-    </>
+    <Routes>
+      <Route index element={<Navigate to="/products" replace />} />
+      <Route path="/products" element={<ProductPage />} />
+      <Route path="*" element={<NotFoundPage />} />
+      <Route path="/details" element={<DetailsPage />} />
+      <Route path="/checkout" element={<CheckoutPage />} />
+    </Routes>
   );
 }
 
---
+++ src/pages/404.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function NotFoundPage() {
+  return (
+    <div>
+      NotFoundPage
+      <h1>404</h1>
+    </div>
+  );
+}
+
+export default NotFoundPage;
